218dab7285a3c7ab66df2e2e19a5fe1a0de6cefe
Arthur Nogueira <arthur.nogueira.ec@gmail.com>
2021-09-22 19:46:32 -0400

    Working with sata stream

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"time"
 
 	"github.com/arthutlnf/fc2-grpc/pb"
 	"google.golang.org/grpc"
@@ -19,7 +21,10 @@ func main() {
 
 	client := pb.NewUserServiceClient(connection)
 
-	AddUser(client)
+	//AddUser(client)
+
+	//AddUserVerbose(client)
+	AddUsers(client)
 
 }
 
@@ -35,6 +40,77 @@ func AddUser(client pb.UserServiceClient) {
 
 	if err != nil {
 		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	fmt.Println(res)
+
+}
+
+func AddUserVerbose(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "Pedro",
+		Email: "pedro@gmail.com",
+	}
+
+	responseStream, err := client.AddUserVerbose(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	for {
+		stream, err := responseStream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Could not receive the message: %v", err)
+		}
+		fmt.Println("Status: ", stream.Status, " - ", stream.GetUser())
+	}
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "w1",
+			Name:  "Wesley 1",
+			Email: "w@w.com",
+		},
+		&pb.User{
+			Id:    "w2",
+			Name:  "Wesley 2",
+			Email: "w2@w.com",
+		},
+		&pb.User{
+			Id:    "w3",
+			Name:  "Wesley 3",
+			Email: "w3@w.com",
+		},
+		&pb.User{
+			Id:    "w4",
+			Name:  "Wesley 4",
+			Email: "w4@w.com",
+		},
+		&pb.User{
+			Id:    "w5",
+			Name:  "Wesley 5",
+			Email: "w5@w.com",
+		},
+	}
+	stream, err := client.AddUsers(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error receiving response: %v", err)
 	}
 
 	fmt.Println(res)
